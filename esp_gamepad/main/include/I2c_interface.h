2ce6baa360614d08ebc629daa7af2aeadfe7498f
haonhien3106@gmail.com <haonhien3106@gmail.com>
2025-01-04 18:01:18 +0700

    file structure modifications

--- esp_gamepad/main/include/I2c_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef ESP_IDF_I2C_INTERFACE_H
-#define ESP_IDF_I2C_INTERFACE_H
-
-void sensor_read_reg_single(uint8_t addr, uint8_t reg, uint8_t *data_out);
-void sensor_read_reg_multi(uint8_t addr, uint8_t start_reg, uint8_t *data_out, uint8_t len);
-void sensor_write_reg_single(uint8_t addr, uint8_t reg, uint8_t data);
-
-#endif
