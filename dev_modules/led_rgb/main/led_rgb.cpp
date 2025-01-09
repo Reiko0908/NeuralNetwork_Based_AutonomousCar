f5643fc27a7ea20c519108db93843fc8b5086596
haonhien3106@gmail.com <haonhien3106@gmail.com>
2025-01-10 02:26:54 +0700

    gamepad done

--- dev_modules/led_rgb/main/led_rgb.cpp
+++ dev_modules/led_rgb/main/led_rgb.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "include/ws2812b.h"
 
 extern "C" void app_main(void)
 {
   Ws2812b led(GPIO_NUM_48);
   led.write(255, 0, 0);
+  while(true){
+    printf("Helloworld\n");
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
